3e794398453b29ceb1bfcb9552a4e03ce6cb887f
kaankaplan111@gmail.com <kaankaplan111@gmail.com>
2022-02-26 22:26:38 +0300

    CinZodyagi is added

---
+++ src/ÇinZodyagi/Main.java
@@ -0,0 +1,64 @@
+package ÇinZodyagi;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Doðum yýlýnýzý giriniz: ");
+		int bornYear = sc.nextInt();
+		
+		switch (bornYear % 12) {
+		case 0: {
+			System.out.println("Maymun");
+			break;
+		}
+		case 1: {
+			System.out.println("Horoz");
+			break;
+		}
+		case 2: {
+			System.out.println("Köpek");
+			break;
+		}
+		case 3: {
+			System.out.println("Domuz");
+			break;
+		}
+		case 4: {
+			System.out.println("Fare");
+			break;
+		}
+		case 5: {
+			System.out.println("Öküz");
+			break;
+		}
+		case 6: {
+			System.out.println("Kaplan");
+			break;
+		}
+		case 7: {
+			System.out.println("Tavþan");
+			break;
+		}
+		case 8: {
+			System.out.println("Ejderha");
+			break;
+		}
+		case 9: {
+			System.out.println("Yýlan");
+			break;
+		}
+		case 10: {
+			System.out.println("At");
+			break;
+		}
+		case 11: {
+			System.out.println("Koyun");
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + bornYear % 12);
+		}
+	}
+}

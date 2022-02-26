5f81e655d2a820bc75b2633f3145bf7802876665
kaankaplan111@gmail.com <kaankaplan111@gmail.com>
2022-02-26 21:11:26 +0300

    SicakligaGoreEtkinlik is added

---
+++ src/HavaEtkinligineGöreEtkinlik/Main.java
@@ -0,0 +1,24 @@
+package HavaEtkinligineGöreEtkinlik;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Sýcaklýðý giriniz: ");
+		int heat = sc.nextInt();
+		
+		if (heat < 5) {
+			System.out.println("Kayak yapabilirsin.");
+		}
+		if (heat > 5 && heat < 15) {
+			System.out.println("Sinemaya gidebilirsin.");
+		}
+		if (heat > 10 && heat < 25) {
+			System.out.println("Pikniðe gidebilirsin.");
+		}
+		if (heat > 25) {
+			System.out.println("Yüzmeye gidebilirsin.");
+		}
+	}
+}

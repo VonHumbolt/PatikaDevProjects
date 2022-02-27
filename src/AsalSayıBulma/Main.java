51468b163e6b6a0fee4a4508a8d2c33a22c3d3a5
kaankaplan111@gmail.com <kaankaplan111@gmail.com>
2022-02-28 01:33:35 +0300

    AsalSayiHesap is added

---
+++ src/AsalSayıBulma/Main.java
@@ -0,0 +1,21 @@
+package AsalSayıBulma;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		for (int i=2; i<=100; i++) {
+			boolean isPrime = true;
+			
+			for (int j=2; j<i; j++) {
+				if (i % j == 0 && i != 2) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) {
+				System.out.print(i + " ");
+			}
+			
+		}
+	}
+}

22896d620910dfdfc90a34d407146169bb7fbcde
kaankaplan111@gmail.com <kaankaplan111@gmail.com>
2022-02-28 15:57:15 +0300

    FibonacciSerisi is added

---
+++ src/FibonacciSerisi/Main.java
@@ -0,0 +1,22 @@
+package FibonacciSerisi;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Fibonacci Serisinin Eleman Sayýsý: ");
+		int number = sc.nextInt();
+		
+		int firstItem = 0;
+		int secondItem = 1;
+		
+		for (int i=1; i<number; i++) {
+			int result = firstItem+secondItem;
+			System.out.println(firstItem + " + " + secondItem + " = " + result);
+			firstItem = secondItem;
+			secondItem = result;
+		}
+		
+	}
+}

969f80661ea68437a56e46f02ee8d05940def645
kaankaplan111@gmail.com <kaankaplan111@gmail.com>
2022-03-17 18:01:32 +0300

    Odv-3 is added

---
+++ src/SQL/odev3.sql
@@ -0,0 +1,4 @@
+select * from country where country like 'A%a';
+select * from country where country like '______n%';
+select * from film where title ilike '%T%T%T%T%';
+select * from film where title like 'C%' and length > 90 and rental_rate = 2.99;
